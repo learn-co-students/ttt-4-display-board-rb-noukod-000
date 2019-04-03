c5dd31a219b67f6cc630513192122c18f3a941e0
Ricardo Paul <kingupper0@gmail.com>
2019-04-03 15:21:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# WE CREATE AN ARRAY WITH 9 EMPTY STRINGS
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# IN THE METHOD, WE SHAPE HOW THE EMPTY STRINGS WILL DISPLAY
+# THREE SPACES SEPARED BY PIPES
+def display_board(board)
+  puts" #{board[0]} | #{board[1]} | #{board[2]} "
+  puts"-----------"
+  puts" #{board[3]} | #{board[4]} | #{board[5]} "
+  puts"-----------"
+  puts" #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
